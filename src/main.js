1feaec2c8ce141002aa62e0892f83300770354c1
pwrstudio <pwrpntr@gmail.com>
2020-10-17 20:43:05 +0200

    Add sentry

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import App from "./App.svelte"
 
+Sentry.init({
+  dsn: 'https://33441033b2d041338bd3b97dc046db7b@o433904.ingest.sentry.io/5467904'});
+
 const app = new App({
   target: document.body,
 })
